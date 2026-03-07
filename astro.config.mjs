96550b1cfd3f0ed0c7f3fdc3420c50621e7ae5bb
70853n <info@code708.com>
2026-03-07 17:35:21 +0100

    chore: Add site to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://code708.github.io/easy-cv/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
